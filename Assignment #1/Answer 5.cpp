a97739c5f767084c0203b92fbefc4992f58c190d
r3dh4r7 <r3dh4r7@CYPHERT>
2015-05-22 21:11:22 +0200

    Uploaded

---
+++ Assignment #1/Answer 5.cpp
@@ -0,0 +1,75 @@
+/*
+Name: Ebi Amanda Ekwok
+Department: Computer Science
+Reg. No: PDS/2013/3976
+
+Assignment #1 answer 5
+*/
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+
+{
+    int num;
+	
+	num=0;
+	cout<<"\nnumber\tsquare\tcube\n"<<num<<"\t"<<num*num<<"\t"<<num*num*num<<"\n";
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	num=num+1;
+	cout<<num<<"\t"<<num*num<<"\t"<<
+	num*num*num<<"\n";
+	
+	
+	
+	return 0;
+}
+
+
